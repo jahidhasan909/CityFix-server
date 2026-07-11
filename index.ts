946b55a803d8d4391430cfae1db5f5bd65637944
jahidhasan909 <jjjjahidhasan999@gmail.com>
2026-07-11 20:54:06 +0600

    mongodb database connect successfully done

--- index.ts
+++ index.ts
@@ -1,8 +1,9 @@
 import express from "express";
 import type { Express } from "express";
-// import { MongoClient, ServerApiVersion, ObjectId } from "mongodb";
+import { MongoClient, ServerApiVersion, ObjectId } from "mongodb";
 import cors from "cors";
 import dotenv from "dotenv";
+
 
 dotenv.config();
 
@@ -14,12 +15,39 @@ const port: number = Number(process.env.PORT) || 8000;
 app.use(cors());
 app.use(express.json())
 
+const uri = process.env.MONGODB_URI
+
+const client = new MongoClient(uri!, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
+});
+
+
+
+async function run() {
+    try {
+        await client.connect();
+        await client.db("admin").command({ ping: 1 });
+        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    } finally {
+
+        // await client.close();
+    }
+}
+run().catch(console.dir);
+
+
+
+
 
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+    res.send('Hello World!')
 })
 
 app.listen(port, () => {
-  console.log(`Example app listening on PORT ${port}`)
+    console.log(`Example app listening on PORT ${port}`)
 })
